836f4b3584925ae1cb309c40bb60ea300d7b0038
Mauricio Miyamura <Mauricio Miyamura>
2016-11-07 08:45:43 -0200

    Create options menu to insert new product

--- app/src/main/java/com/yuddi/inventoryapp/MainActivity.java
+++ app/src/main/java/com/yuddi/inventoryapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.yuddi.inventoryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,4 +21,26 @@ public class MainActivity extends AppCompatActivity {
         productList.setAdapter(mCursorAdapter);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_insert_new_product:
+                insertNewProduct();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void insertNewProduct() {
+        
+    }
+
 }
