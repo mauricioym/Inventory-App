deaf7c7249b004ebf3029f336798eddd2f686c5b
Mauricio Miyamura <Mauricio Miyamura>
2016-11-07 11:46:06 -0200

    Implement Loader Callbacks on MainActivity

--- app/src/main/java/com/yuddi/inventoryapp/MainActivity.java
+++ app/src/main/java/com/yuddi/inventoryapp/MainActivity.java
@@ -1,14 +1,22 @@
 package com.yuddi.inventoryapp;
 
+import android.database.Cursor;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import com.yuddi.inventoryapp.data.InventoryContract.InventoryEntry;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private InventoryCursorAdapter mCursorAdapter;
+
+    private static final int INVENTORY_LOADER = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         mCursorAdapter = new InventoryCursorAdapter(this, null);
         productList.setAdapter(mCursorAdapter);
 
+        getSupportLoaderManager().initLoader(INVENTORY_LOADER, null, this);
     }
 
     @Override
@@ -40,7 +49,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void insertNewProduct() {
-        
+
     }
 
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        switch (id) {
+            case INVENTORY_LOADER:
+
+                String[] projection = {
+                        InventoryEntry._ID,
+                        InventoryEntry.COLUMN_INVENTORY_NAME,
+                        InventoryEntry.COLUMN_INVENTORY_QUANTITY,
+                        InventoryEntry.COLUMN_INVENTORY_PRICE
+                };
+
+                return new CursorLoader(
+                        this,
+                        InventoryEntry.CONTENT_URI,
+                        projection,
+                        null,
+                        null,
+                        null
+                );
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        mCursorAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mCursorAdapter.swapCursor(null);
+    }
 }
