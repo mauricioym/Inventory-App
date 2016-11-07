89ce0e3e34e6fbe7f395c569bdc7ee048b9617c4
Mauricio Miyamura <Mauricio Miyamura>
2016-11-07 20:07:31 -0200

    Add Detail activity

--- app/src/main/java/com/yuddi/inventoryapp/MainActivity.java
+++ app/src/main/java/com/yuddi/inventoryapp/MainActivity.java
@@ -1,14 +1,19 @@
 package com.yuddi.inventoryapp;
 
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
+import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.yuddi.inventoryapp.data.InventoryContract.InventoryEntry;
@@ -29,6 +34,16 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         mCursorAdapter = new InventoryCursorAdapter(this, null);
         productList.setAdapter(mCursorAdapter);
 
+        productList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Log.i("oie","porra");
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                intent.setData(ContentUris.withAppendedId(InventoryEntry.CONTENT_URI, id));
+                startActivity(intent);
+            }
+        });
+
         getSupportLoaderManager().initLoader(INVENTORY_LOADER, null, this);
     }
 
@@ -41,8 +56,8 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_insert_new_product:
-                insertNewProduct();
+            case R.id.action_add_product:
+                addProduct();
                 return true;
             case R.id.action_insert_dummy_data:
                 insertDummyData();
@@ -55,8 +70,9 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         return super.onOptionsItemSelected(item);
     }
 
-    private void insertNewProduct() {
-
+    private void addProduct() {
+        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+        startActivity(intent);
     }
 
     private void insertDummyData() {
