dd44a0e91645de5bd6a291ee8ebdeda8b05045fb
Mauricio Miyamura <Mauricio Miyamura>
2016-11-08 01:32:45 -0200

    Now the user can pick an image from gallery

--- app/src/main/java/com/yuddi/inventoryapp/DetailActivity.java
+++ app/src/main/java/com/yuddi/inventoryapp/DetailActivity.java
@@ -1,16 +1,24 @@
 package com.yuddi.inventoryapp;
 
 import android.content.ContentValues;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.yuddi.inventoryapp.data.InventoryContract.InventoryEntry;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -22,7 +30,7 @@ public class DetailActivity extends AppCompatActivity {
     private EditText mPhoneEditText;
     private EditText mEmailEditText;
 
-    private int oldImage = 0;
+    private boolean imageChanged = false;
     private String oldName = "";
     private int oldQuantity = 0;
     private int oldPrice = 0;
@@ -30,13 +38,15 @@ public class DetailActivity extends AppCompatActivity {
     private int oldPhone = 0;
     private String oldEmail = "";
 
-    private int newImage;
+    private Bitmap newPicture;
     private String newName;
     private int newQuantity;
     private int newPrice;
     private String newDescription;
     private int newPhone;
     private String newEmail;
+
+    private static final int PICK_IMAGE_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +63,43 @@ public class DetailActivity extends AppCompatActivity {
         mPhoneEditText = (EditText) findViewById(R.id.detail_phone_edittext);
         mEmailEditText = (EditText) findViewById(R.id.detail_email_edittext);
 
+        mPictureImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select a picture from"), PICK_IMAGE_REQUEST);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                newPicture = scaleBitmap(bitmap);
+                mPictureImageView.setImageBitmap(newPicture);
+                mPictureImageView.setBackgroundColor(Color.TRANSPARENT);
+                imageChanged = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Bitmap scaleBitmap(Bitmap bitmap) {
+        int height = mPictureImageView.getHeight();
+        int width = height * bitmap.getWidth() / bitmap.getHeight();
+
+        return Bitmap.createScaledBitmap(bitmap, width, height, false);
     }
 
     @Override
@@ -98,7 +145,8 @@ public class DetailActivity extends AppCompatActivity {
 
         return (!newName.equals(oldName)) || (newQuantity != oldQuantity)
                 || (newPrice != oldPrice) || (!newDescription.equals(oldDescription))
-                || (newPhone != oldPhone) || (!newEmail.equals(oldEmail));
+                || (newPhone != oldPhone) || (!newEmail.equals(oldEmail))
+                || imageChanged;
 
     }
 
@@ -109,6 +157,7 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         ContentValues values = new ContentValues();
+        values.put(InventoryEntry.COLUMN_INVENTORY_PICTURE, convertToByteArray(newPicture));
         values.put(InventoryEntry.COLUMN_INVENTORY_NAME, newName);
         values.put(InventoryEntry.COLUMN_INVENTORY_QUANTITY, newQuantity);
         values.put(InventoryEntry.COLUMN_INVENTORY_PRICE, newPrice);
@@ -123,4 +172,11 @@ public class DetailActivity extends AppCompatActivity {
             Toast.makeText(this, R.string.detail_add_product_successful, Toast.LENGTH_SHORT).show();
         }
     }
+
+    private byte[] convertToByteArray(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream);
+        return stream.toByteArray();
+    }
+
 }
